051058720bb0396d15cb5baccee754ed6b958923
Shaotong Sun <sunshaotong0713@gmail.com>
2023-07-11 16:12:26 +0800

    added features
    read from config files, logging

--- src/corun.rs
+++ src/corun.rs
@@ -1,28 +1,118 @@
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 use std::path::PathBuf;
+// use clap::error::ContextValue::String;
+use std::string::String;
 
-// TODO: need logging(tracing and tracing-sub) on each program start time and finish time and the order thy ran, continuous scheduling, randomly schedule multiple programs
+use tracing::{info, trace};
+
+// TODO: need logging(tracing and tracing-sub) on each program start time and finish time and the order thy ran(√), continuous scheduling, randomly schedule multiple programs(i actually didn't feel the need for this)
 // TODO: need to add git submodule for polybench
-// FIXME: probably more helpful if we read in program paths and arguments from a file
+// FIXME: probably more helpful if we read in program paths and arguments from a file(√)
+// TODO: do permutation on the program args and other programs, and then use them as program pools
+// TODO: need to add back tracing for the program start time and finish time using logging in order to tell which programs are running at given time, thus can figure out the DMC for that given time
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Program {
     pub(crate) path: Option<PathBuf>,
     pub(crate) args: Option<Vec<String>>,
 }
 
-pub fn co_run(programs: Vec<Program>) -> Vec<std::time::Duration> {
+fn logging(
+    current_prog_status: usize,
+    timer: &HashMap<u32, (std::time::Instant, Program)>,
+    curr_prog: &Program,
+    curr_pid: u32,
+    start: std::time::Instant,
+    log_file: &mut File,
+) {
+    match current_prog_status {
+        0 => {
+            // 0 for start
+            trace!(
+                "program {:?}(pid: {:?}) started with {:?} args at {:?} time",
+                curr_prog.path,
+                curr_pid,
+                curr_prog.args,
+                start
+            );
+        }
+        1 => {
+            // 1 for finish
+            trace!(
+                "program {:?}(pid: {:?}) finished with {:?} args at {:?} time",
+                curr_prog.path,
+                curr_pid,
+                curr_prog.args,
+                start
+            );
+        }
+        _ => {
+            panic!("current program status is not 0 or 1");
+        }
+    }
+    info!(
+        "current running programs' starting time, names and args: {:#?}",
+        timer.values()
+    );
+    log_file
+        .write_all(
+            format!(
+                "current running programs' starting time, names and args: {:#?}",
+                timer.values()
+            )
+            .as_bytes(),
+        )
+        .expect("unexpected error");
+}
+
+pub fn co_run(programs: Vec<Program>) -> HashMap<Program, std::time::Duration> {
+    let mut log_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("log");
+    let mut timer = HashMap::new();
     let mut children = Vec::new();
+    let mut logger = HashMap::new();
     for program in programs {
-        let child = single_run(program);
+        let child = single_run(program.clone());
+        let pid = child.id();
+        let start = std::time::Instant::now(); // FIXME: should this be here or right after the .wait()?
+        logging(
+            0,
+            &timer,
+            &program,
+            pid,
+            start,
+            log_file
+                .as_mut()
+                .unwrap_or_else(|_| panic!("log file is not created")),
+        );
+        timer.insert(pid, (start, program.clone()));
         children.push(child);
     }
-    let mut durations = Vec::new();
     for mut child in children {
-        let start = std::time::Instant::now();
         let _ = child.wait().expect("command wasn't running");
+        let pid = child.id();
+        let (start, program) = timer
+            .remove(&pid)
+            .unwrap_or_else(|| panic!("program with pid: {:?} is not in the timer", child.id()));
         let duration = start.elapsed();
-        durations.push(duration);
+        logging(
+            1,
+            &timer,
+            &program,
+            pid,
+            start,
+            log_file
+                .as_mut()
+                .unwrap_or_else(|_| panic!("log file is not created")),
+        );
+        logger.insert(program, duration);
     }
-    durations
+    logger
 }
 
 #[inline(always)]
@@ -30,9 +120,6 @@ fn single_run(program: Program) -> std::process::Child {
     let Some(path) = program.path else {
         panic!("program path is not specified");
     };
-    // let Some(args) = program.args.clone() else {
-    //
-    // };
     let args = program.args.unwrap_or(Vec::new());
     std::process::Command::new(path)
         .args(args)
@@ -41,8 +128,4 @@ fn single_run(program: Program) -> std::process::Child {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    // #[test]
-}
+mod tests {}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use clap::Parser;
+use std::fs;
 use std::path::PathBuf;
+
+// use tracing_subscriber::prelude::*;
 mod corun;
 
-// TODO: need to change this to a file input
+// TODO: need to change this to a file input (√)
 
 #[derive(Debug, Parser)]
-pub struct CliOpt {
+pub struct CliInput {
     #[arg(long, short, help = "program path")]
     pub path: Option<PathBuf>,
     #[arg(long, short, help = "program arguments")]
@@ -13,23 +16,57 @@ pub struct CliOpt {
 }
 
 #[derive(Debug, Parser)]
+pub struct FileInput {
+    #[arg(long, short, help = "config file real path")]
+    pub config_path: Option<PathBuf>,
+}
+
+#[derive(Debug, Parser)]
 enum Command {
     #[allow(non_camel_case_types)]
     #[command(about = "co-run programs real paths and arguments")]
-    corun(CliOpt),
+    line_args(CliInput),
+    #[allow(non_camel_case_types)]
+    #[command(about = "real path to the config file of the co-run programs")]
+    file_args(FileInput),
 }
 
 fn main() -> anyhow::Result<()> {
     env_logger::init_from_env("CORUN_LOG");
     let command: Command = Command::parse();
     match command {
-        Command::corun(opt) => {
+        Command::line_args(opt) => {
             let programs = vec![corun::Program {
                 path: opt.path,
                 args: opt.args,
             }];
             let durations = corun::co_run(programs);
-            println!("durations: {:?}", durations);
+            println!("durations: {:#?}", durations);
+        }
+        Command::file_args(opt) => {
+            let config_path = opt
+                .config_path
+                .unwrap_or_else(|| panic!("config file path is not specified"));
+            // get all the lines from the file
+            let contents = fs::read_to_string(config_path)?;
+            let mut programs = Vec::new();
+            for line in contents.lines() {
+                //hope this works
+                let mut program = corun::Program {
+                    path: None,
+                    args: None,
+                };
+                let mut iter = line.split_whitespace();
+                let path = iter
+                    .next()
+                    .unwrap_or_else(|| panic!("program path is not specified"));
+                let args = iter.collect::<Vec<&str>>();
+                program.path = Some(PathBuf::from(path));
+                program.args = Some(args.iter().map(|s| s.to_string()).collect::<Vec<String>>());
+                programs.push(program);
+            }
+            let durations = corun::co_run(programs);
+            println!("durations: {:#?}", durations);
         }
     }
     Ok(())
